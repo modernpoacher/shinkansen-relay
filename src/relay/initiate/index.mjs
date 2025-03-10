86093ccc171c105dbe7d77cecc4ffe795950bfcf
Jonathan Perry <jonathanperry@modernpoacher.com>
2025-03-10 21:32:06 +0000

    Removes `Reflect`

--- src/relay/initiate/index.mjs
+++ src/relay/initiate/index.mjs
@@ -28,19 +28,19 @@ export default function initiate ({
     OMEGA
   } = Signals
 
-  const alpha = now[ALPHA] // Reflect.get(now, ALPHA)
-  const omega = now[OMEGA] // Reflect.get(now, OMEGA)
+  const alpha = now[ALPHA]
+  const omega = now[OMEGA]
 
   const HAS_ALPHA = !!alpha
   const HAS_OMEGA = !!omega
 
   log(now, HAS_ALPHA, HAS_OMEGA)
 
-  const HAS_ALPHA_CHANGED = was[ALPHA] !== alpha // Reflect.get(was, ALPHA) !== alpha
-  const HAS_OMEGA_CHANGED = was[OMEGA] !== omega // Reflect.get(was, OMEGA) !== omega
+  const HAS_ALPHA_CHANGED = was[ALPHA] !== alpha
+  const HAS_OMEGA_CHANGED = was[OMEGA] !== omega
 
-  const WAS_ALPHA_DEFINED = ALPHA in was // Reflect.has(was, ALPHA)
-  const WAS_OMEGA_DEFINED = OMEGA in was // Reflect.has(was, OMEGA)
+  const WAS_ALPHA_DEFINED = ALPHA in was
+  const WAS_OMEGA_DEFINED = OMEGA in was
 
   return {
     HAS_ALPHA,
